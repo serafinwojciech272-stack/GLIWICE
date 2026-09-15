2e5c48d71a7de36fae0ebdc6642180a382ffd7b2
serafinwojciech272-stack <serafinwojciech272@gmail.com>
2026-09-15 09:48:15 +0200

    feat(szpieg): add transparent resale cost assumptions

---
+++ src/config/profitAssumptions.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_PROFIT_ASSUMPTIONS = {
+  marketplaceFeePct: 8,
+  paymentFeePct: 1.5,
+  packagingCost: 8,
+  shippingIn: 0,
+  otherCosts: 0,
+} as const;
+
+export const PROFIT_ASSUMPTION_LABEL = 'Model bazowy: 8% marketplace + 1.5% płatność + 8 zł pakowanie. Zmień przed decyzją zakupową.';
