3497733a58acc8da8fe24fc09ad5d05ae5fb30d0
serafinwojciech272-stack <serafinwojciech272@gmail.com>
2026-09-23 15:45:05 +0200

    fix(ui): mount application error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,1 @@
-import React from 'react';import{createRoot}from'react-dom/client';import'./index.css';import App from'./App';createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
+import React from 'react';import{createRoot}from'react-dom/client';import'./index.css';import App from'./App';import { AppErrorBoundary } from './AppErrorBoundary';createRoot(document.getElementById('root')!).render(<React.StrictMode><AppErrorBoundary><App/></AppErrorBoundary></React.StrictMode>);
